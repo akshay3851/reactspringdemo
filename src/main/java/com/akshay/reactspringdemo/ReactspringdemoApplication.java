91c99db28707d7d3ccb4e0aa2cd4279c60b3f8d4
akshay3851 <akshay.kumar3851@gmail.com>
2019-10-03 11:41:05 +0530

    Spring Boot and React first commit

---
+++ src/main/java/com/akshay/reactspringdemo/ReactspringdemoApplication.java
@@ -0,0 +1,13 @@
+package com.akshay.reactspringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactspringdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactspringdemoApplication.class, args);
+	}
+
+}
